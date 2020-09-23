521f47fb81942e1abb29c5e5940b9bd535a48ee3
Vinicius Faria <vinicius_faria.s@hotmail.com>
2020-09-23 17:55:00 -0300

    OrganizacaoJS

--- Exercicios-js/fundamentos/organizacao.js
+++ Exercicios-js/fundamentos/organizacao.js
@@ -1,7 +1,18 @@
-//Javascript é organizado em sentenças de códigos
+//Javascript é organizado em sentenças de códigos que podem ou não terminar com ;
 
-console.log("Sentença de código"); //Pode ou não terminar com ;
+console.log("Sentença de código 1"); 
+console.log("Sentença de código 2")
 
 {
-    //Bloco de código em JS
+    //Bloco de código em Javascript
+}
+
+{
+    {
+        {
+            //Podem existir blocos aninhados
+            console.log("Olá")
+            console.log("Mundo!!")
+        }
+    }
 }
